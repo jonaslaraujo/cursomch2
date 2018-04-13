f415081d49dab885dfd6ef487b2a9e6ebf810e0b
Jonas Araujo <jonaslaraujo@gmail.com>
2018-04-13 11:54:04 -0300

    Projeto Criado

---
+++ src/main/java/br/com/jonasaraujo/cursomc/Cursomch2Application.java
@@ -0,0 +1,12 @@
+package br.com.jonasaraujo.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomch2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomch2Application.class, args);
+	}
+}
