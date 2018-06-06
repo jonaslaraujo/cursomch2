4c6a83f9e43a50927a7182047f88b478363a5a10
Jonas Araujo <jonaslaraujo@gmail.com>
2018-06-06 15:33:49 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/br/com/jonasaraujo/cursomc/domain/Cliente.java
+++ src/main/java/br/com/jonasaraujo/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import br.com.jonasaraujo.cursomc.domain.enums.TipoCliente;
@@ -39,6 +40,7 @@ public class Cliente implements Serializable {
 	private Set<String> telefones = new HashSet<>();
 	
 	@OneToMany(mappedBy="cliente")
+	@JsonIgnore
 	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
