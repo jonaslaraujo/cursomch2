1629fc6c57df6a7e7b580c481a656c15b401989d
Jonas Lopes de Araujo <t_jlaraujo@trf5.gov.br>
2019-02-07 16:10:46 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/br/com/jonasaraujo/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/jonasaraujo/cursomc/services/CategoriaService.java
@@ -48,7 +48,7 @@ public class CategoriaService {
 		}
 		catch (DataIntegrityViolationException e) 
 		{
-			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
 		}
 	}
 	
