9f158ad5e20a8b63c7c9592b397d9c65d02e6c9f
Jonas Lopes de Araujo <t_jlaraujo@trf5.gov.br>
2019-02-07 10:57:03 -0300

    Paginacao com parametros opicionais na requisicao

--- src/main/java/br/com/jonasaraujo/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/jonasaraujo/cursomc/services/CategoriaService.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.jonasaraujo.cursomc.domain.Categoria;
-import br.com.jonasaraujo.cursomc.dto.CategoriaDTO;
 import br.com.jonasaraujo.cursomc.repositories.CategoriaRepository;
 import br.com.jonasaraujo.cursomc.services.exceptions.DataIntegrityException;
 import br.com.jonasaraujo.cursomc.services.exceptions.ObjectNotFoundException;
@@ -50,5 +52,10 @@ public class CategoriaService {
 	public List<Categoria> findAll()
 	{
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
