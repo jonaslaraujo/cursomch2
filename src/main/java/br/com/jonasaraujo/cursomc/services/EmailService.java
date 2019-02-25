e6b40825cdf89ad367dcd60493da02ffadab2fdf
Jonas Lopes de Araujo <t_jlaraujo@trf5.gov.br>
2019-02-25 16:17:05 -0300

    email html

--- src/main/java/br/com/jonasaraujo/cursomc/services/EmailService.java
+++ src/main/java/br/com/jonasaraujo/cursomc/services/EmailService.java
@@ -1,6 +1,9 @@
 package br.com.jonasaraujo.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
+
 import br.com.jonasaraujo.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -8,5 +11,9 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 	
 }
